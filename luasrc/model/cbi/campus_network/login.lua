26519254d5d48fae241114feba56a419f0f5813b
YL2209 <3021027861@qq.com>
2025-02-26 02:53:04 +0800

    npm publish

--- luasrc/model/cbi/campus_network/login.lua
+++ luasrc/model/cbi/campus_network/login.lua
@@ -1,54 +1,70 @@
+require("luci.sys")
+
 m = Map("campus_network", translate("Campus Network Login Settings"),
-    translate("Configure parameters for campus network authentication and scheduling."))
+    translate('Configure parameters for campus network authentication and scheduling.<br /><br />Please refer to the document for details:<a href="https://blog.naokuo.top/p/522a17f8.html" target="_blank">Click to view document</a>'));
 
-s = m:section(NamedSection, "login", "login", translate("Login Parameters"))
-s.addremove = false
+s = m:section(TypedSection, "login", translate("Login Parameters"));
+s.anonymous = true;
 
-help_link = s:option(DummyValue, "help_link", translate("Documentation"))
-help_link.rawhtml = true
-help_link.value = '<a href="https://blog.naokuo.top/p/522a17f8.html" target="_blank">'..translate("Click to view documentation")..'</a>'
-    
-s:option(Value, "username", translate("Username")).rmempty = false
-pass = s:option(Value, "pwd", translate("Password"))
-pass.password = true
-pass.rmempty = false
-wlanuserip = s:option(Value, "wlanuserip", translate("WLAN User IP"))
-wlanuserip.rmempty = false
-wlanuserip.placeholder = "wlanuserip"
-nasip = s:option(Value, "nasip", translate("NAS IP"))
-nasip.rmempty = false
-nasip.placeholder = "nasip"
-s:option(Value, "login_url", translate("Login URL")).rmempty = false
-ping_ip = s:option(Value, "ping_ip", translate("Ping Test IP"), translate("IP address for network connectivity detection (e.g. 114.114.114.114)"))
-ping_ip.datatype = "ip4addr"  -- IP格式验证
-ping_ip.rmempty = false       -- 必填项
-s:option(Value, "max_attempts", translate("Max Attempts")).datatype = "uinteger"
+username = s:option(Value, "username", translate("Username"), translate('Usually for your student ID.'));
+username.rmempty = false;
 
--- Cron Schedule
-cron = s:option(Value, "cron_schedule", translate("Cron Schedule"), translate("Cron expression (e.g. '*/1 * * * *' for every 1 minutes)"))
-cron.rmempty = false
+pwd = s:option(Value, "pwd", translate("Password"));
+pwd.password = true;
+pwd.rmempty = false;
 
-function m.on_after_commit(self)
-    local uci = require "luci.model.uci".cursor()
-    local cron_schedule = uci:get("campus_network", "login", "cron_schedule") or ""
-    local script_path = "/usr/libexec/campus_login"
+wlanuserip = s:option(Value, "wlanuserip", translate("WLAN User IP"));
+wlanuserip.rmempty = false;
+wlanuserip.placeholder = "wlanuserip";
 
-    -- 安全删除旧任务
-    os.execute("crontab -l | grep -v '"..script_path.."' | crontab -")
-    
-    if cron_schedule ~= "" and cron_schedule ~= nil then
-        -- 验证Cron表达式格式
-        if not cron_schedule:match("%S+ %S+ %S+ %S+ %S+") then
-            luci.http.redirect(luci.dispatcher.build_url("admin/services/campus_network"))
-            return
-        end
-        
-        -- 安全添加新任务
-        os.execute("(crontab -l; echo '"..cron_schedule.." "..script_path.."') | crontab -")
-        
-        -- 重启cron服务
-        os.execute("/etc/init.d/cron restart >/dev/null 2>&1")
-    end
+nasip = s:option(Value, "nasip", translate("Network Access Server IP"));
+nasip.rmempty = false;
+nasip.placeholder = "nasip";
+
+url = s:option(Value, "login_url", translate("Login URL"));
+url.rmempty = false;
+
+ping = s:option(Value, "ping_ip", translate("Ping Test IP"), 
+    translate("IP address for network connectivity detection (e.g. 114.114.114.114)"));
+ping.datatype = "ip4addr";
+ping.rmempty = false;
+
+attempts = s:option(Value, "max_attempts", translate("Max Attempts"));
+attempts.datatype = "uinteger";
+attempts.rmempty = false;
+
+
+cron = m:section(TypedSection, "cron", translate("Plan tasks."))
+cron.addremove = false
+cron.anonymous = true
+
+enable = cron:option(Flag,"enable", translate("Enable"))
+enable.rmempty = false
+enable.default = 0
+
+week = cron:option(ListValue, "week", translate("Week Day"))
+week:value(7, translate("Everyday"))
+week:value(1, translate("Monday"))
+week:value(2, translate("Tuesday"))
+week:value(3, translate("Wednesday"))
+week:value(4, translate("Thursday"))
+week:value(5, translate("Friday"))
+week:value(6, translate("Saturday"))
+week:value(0, translate("Sunday"))
+week.default = 0
+
+hour = cron:option(Value, "hour", translate("Hour"))
+hour.datatype = "range(0,23)"
+hour.rmempty = false
+
+minute = cron:option(Value, "minute", translate("Minute"))
+minute.datatype = "range(1,59)"
+minute.rmempty = false
+
+
+local e = luci.http.formvalue("cbi.apply")
+if e then
+    io.popen("/etc/init.d/campus_network restart")
 end
 
 return m
